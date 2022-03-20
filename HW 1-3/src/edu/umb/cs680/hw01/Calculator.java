b68f7a4692012c327a8f4a5de2847c0ec71a9ede
VenigallaAbhiram <102000606+VenigallaAbhiram@users.noreply.github.com>
2022-03-20 19:51:30 -0400

    Add files via upload

---
+++ HW 1-3/src/edu/umb/cs680/hw01/Calculator.java
@@ -0,0 +1,25 @@
+package edu.umb.cs680.hw01;
+
+public class Calculator {
+
+	public double multiply(double x,double y) {
+		return (x*y);
+	}
+	
+	public float divide(float x, float y) {
+		if(y==0) {
+			throw new IllegalArgumentException("undefined!!");
+		}else {
+			return (x/y);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Calculator calculator= new Calculator();
+		System.out.println("the value is " +calculator.multiply(1, 3));
+		System.out.println("the value is " +calculator.divide(3, 1));
+		System.out.println("the value is " +calculator.divide(3, 0));
+
+	}
+}
